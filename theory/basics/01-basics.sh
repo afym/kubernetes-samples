2e7b2a67522389724507158f49ccfde80af95f6b
afym <angel.fym@gmail.com>
2019-05-26 21:52:14 -0500

    Adding a new structure for my samples

--- theory/basics/01-basics.sh
+++ theory/basics/01-basics.sh
@@ -9,3 +9,6 @@ kubectl get nodes $node_name
 kubectl get nodes $node_name -o yaml
 
 kubectl describe node $node_name
+
+# join inside a bash terminal in container
+kubectl exec -it <pod-name> -c <container> -- /bin/bash
